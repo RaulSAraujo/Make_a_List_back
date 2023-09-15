334cd7671cf248761c32de30920bafd6483b635c
RaulSAraujo <raul.araujo.2000@hotmail.com>
2023-09-14 23:51:46 -0300

    Fix: comment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//cookie middleware
+// Middleware cookie
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
