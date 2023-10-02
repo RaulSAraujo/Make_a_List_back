a00f0c8cd85ca9946ff1e5a78d3e1041c9aa8e4e
RaulSAraujo <raul.araujo.2000@hotmail.com>
2023-10-01 21:19:21 -0300

    Fix: methods cors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
     app.use(cors());
     next();
 });
