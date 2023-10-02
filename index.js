f6077068b48cc6761aad6ead26d43d15bb38baf3
RaulSAraujo <raul.araujo.2000@hotmail.com>
2023-10-01 21:24:49 -0300

    Fix: cors

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ require('dotenv').config()
 const app = express()
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
-    app.use(cors());
-    next();
-});
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
